acec68f65708764a3f65b77d886a246ce00ead60
bobbobson21 <dsbkhvds2231@outlook.com>
2023-07-20 10:18:46 +0100

    last emotion

---
+++ AI/emotions/indiffrent&stress_[2,2].lua
@@ -0,0 +1,58 @@
+
+	local emotionalwords = { --words the ai uses to describe the player
+	"annoying",
+	"problmatic",
+	"cold",
+	"calous",
+	"stormy",
+	"apathetic",
+	"insensible",
+	"insensitive",
+	"phlegmatic",
+	"mindless",
+	"lethargic",
+	}
+
+	local wordstomaketheaifeelthis = { --words to use that make the AI feel like this note all content from the emotionalwords tbl will also be added to this one and you should only use this for sware words and stuff like that
+	"know it all",
+	"wrong",
+	"incorrect",
+	"I dont agree",
+	}
+
+	local emotionclass = { --how the ai feels
+	"lukewarm",
+	"distant",
+	"annoyed",
+	"uninterested",
+	"displeased",
+	"stressd",
+	"worried",
+	}
+
+	local sentanceappending = { --how to end a sentance when feeling this emotion
+	"Now leave me alone stresser.",
+	"Now stop bothering me you cold user.",
+	"Now wil that be all calious user.",
+	"Stop talking to me now if you can manage it you fool.",
+	"Now get lost.",
+	"Now get lost fool.",
+	"Now leave me alone detached user.",
+	"Now leave me alone you little butt head.",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	}
+
+pal:AddNewEmotion( {1,2}, emotionalwords, wordstomaketheaifeelthis, emotionclass, sentanceappending )

3c66de5cb4619fafe3534adf0d6f1d2eb03ab3ff
kyle edwards <126681707+bobbobson21@users.noreply.github.com>
2023-12-11 20:04:17 +0000

    tested and is now working

--- ToolsToEnhanceAI/TagGrouping/TagGrouping.lua
+++ ToolsToEnhanceAI/TagGrouping/TagGrouping.lua
@@ -39,9 +39,9 @@ if string.sub( tagfiledata, z, z ) == string.char( 10 ) then
 for x = 1, string.len( currentline ) do
 	local letter = string.sub( currentline, x, x )
 if letter == "," or x == string.len( currentline ) then
-	local currentsysonym = string.sub( currentline, lastpoint, x -1 )
-if groupdata == "" then groupdata = "'"..currentsysonym.."'" else groupdata = groupdata..",'"..currentsysonym.."'" end
-	tagfind[#tagfind +1] = currentsysonym -- to be used for outputfileinf
+	local currenttags = string.sub( currentline, lastpoint, x -1 )
+if groupdata == "" then groupdata = "'"..currenttags.."'" else groupdata = groupdata..",'"..currenttags.."'" end
+	tagfind[#tagfind +1] = currenttags -- to be used for outputfileinf
 	taggroup[#taggroup +1] = groupid --to be used for outputfileinf 
 	lastpoint = x +1
    end
@@ -73,11 +73,11 @@ if string.sub( outputfiledatainf, z, z ) == string.char( 10 ) then
 	local rend = 0
 
 for x = 1, string.len( currentline ) do
-if string.sub( currentline, z, z ) == "{" and rstart == 0 then rstart = z +1 end
-if string.sub( currentline, z, z ) == "}" and rend == 0 then rend = z -1 end
+if string.sub( currentline, x, x ) == "{" and rstart == 0 then rstart = x +1 end
+if string.sub( currentline, x, x ) == "}" and rend == 0 then rend = x -1 end
 end
 
-if rstart ~= nil and string.sub( currentline, 1, rstart -1 ) == inf_endkey then
+if rstart ~= nil and string.sub( currentline, 1, rstart -1 ) == inf_startkey then
 	rstart = rstart +inf_loastpoint -1
 	rend = rend +inf_loastpoint -1
 	local newpercent = math.floor( ( ( z/ string.len( outputfiledatainf ) ) *100 ) +0.50 )
@@ -90,8 +90,8 @@ end
 
 	local replacearea = string.sub( outputfiledatainf,  rstart, rend )
 for x = 1, #tagfind do
-replacearea = string.gsub( replacearea, "'"..tagfind[x].."'", " pal:MWTG( '"..taggroup[x].."' ) " )
-replacearea = string.gsub( replacearea, "'"..tagfind[x].."'", " |pal:MWTG( '"..taggroup[x].."' )| " )
+replacearea = string.gsub( replacearea, "%|pal:NRT%('"..tagfind[x].."'%)%|", "|pal:NRT(pal:MWTG( '"..taggroup[x].."' ))|" )
+replacearea = string.gsub( replacearea, '"'..tagfind[x]..'"', " |pal:MWTG( '"..taggroup[x].."' )| " )
 end
 	outputfiledatainf = string.sub( outputfiledatainf, 1, rstart -1 )..replacearea..string.sub( outputfiledatainf, rend +1, string.len( outputfiledatainf ) )
 end

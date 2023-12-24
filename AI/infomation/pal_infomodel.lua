921f2cda4c0710deae5c738240e00c0c19d43b05
kyle edwards <126681707+bobbobson21@users.noreply.github.com>
2023-12-24 15:56:53 +0000

    infomodel changes and the last update was bug fixes

--- AI/infomation/pal_infomodel.lua
+++ AI/infomation/pal_infomodel.lua
@@ -1,4 +1,4 @@
---[[-- notice -----------------------------------------------------------------
+--[[-- notice --------------------------------------------------------------------------------------------------------------------------------------------------------------
 This file contains everything the AI knows about or has an opioion of.
 
 The file dose NOT use evey inch of available resources to make responces as
@@ -6,9 +6,9 @@ unique as possible as this is for testing and demostration.
 
 It Is recommended you make your own info model from the ground up with:
 SynonymsGroups, TagGroups (not all but definetly some of the time) and MemGen
----------------------------------------------------------------------------]]--
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------]]--
 
---[[-- test code DON'T put into circulation -----------------------------------
+--[[-- test code DON'T put into circulation --------------------------------------------------------------------------------------------------------------------------------
 
 pal:AddNewSynonymsGroup( "blahaj", {"blahaj the trans shark","shark god","the cute shark","the shark I want and deserve lol"} )
 pal:AddNewInfo( {"test"}, {}, {0,0}, true, 0, {"wow I did it I pased the test heck yer now to be blessed by the great |pal:GetSynonymsWord( 'blahaj' )|"}, {}, nil, nil )
@@ -16,7 +16,7 @@ pal:AddNewInfo( {"test"}, {}, {0,0}, true, 0, {"wow I did it I pased the test he
 pal:AddNewTagGroup( "test_tags", {"test"} )
 pal:SetNewInfo( {"|pal:MWTG( 'test_tags' )|"}, {}, {0,0}, false, true, 0, {"wow I did it I pased the test heck yer now to be blessed by the great blahaj"}, {}, nil, nil )
 
----------------------------------------------------------------------------]]--
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------]]--
 
 pal:SetNewInfo( {"you","|pal:NRT( 'make' )|","|pal:NRT( 'build' )|","|pal:NRT( 'create' )|","|pal:NRT( 'destroy' )|","|pal:NRT( 'remove' )|","|pal:NRT( 'do' )|",}, nil, nil, false, false, nil, {"|pal:SandBox():LearnGeneral()|"}, nil, nil, nil )
 pal:SetNewInfo( {"i","|pal:NRT( 'like' )|","|pal:NRT( 'liked' )|","|pal:NRT( 'favourite' )|","|pal:NRT( 'hate' )|","|pal:NRT( 'dislike' )|","|pal:NRT( 'loth' )|",}, nil, nil, false, false, nil,  {"|pal:SandBox():LearnAboutUser()|"}, nil, nil, nil )
@@ -24,7 +24,7 @@ pal:SetNewInfo( {"i","|pal:NRT( 'like' )|","|pal:NRT( 'liked' )|","|pal:NRT( 'fa
 pal:SetNewInfo( {"what","is","|pal:NRT( '+' )|","|pal:NRT( '-' )|","|pal:NRT( '>' )|","|pal:NRT( '<' )|","|pal:NRT( '/' )|","|pal:NRT( '*' )|",}, nil, nil, false, false, nil, {"that is |pal:SandBox():DoMaths()|"}, nil, nil, nil ) --UNSCURE MUST DESTROY IF USING FOR CONSUMSION OR CUSTOMERS
 pal:SetNewInfo( {"dose","|pal:NRT( '+' )|","|pal:NRT( '-' )|","|pal:NRT( '>' )|","|pal:NRT( '<' )|","|pal:NRT( '/' )|","|pal:NRT( '*' )|",}, nil, nil, false, false, nil, {"|pal:SandBox():CheckMaths()|"}, nil, nil, nil ) --UNSCURE MUST DESTROY IF USING FOR CONSUMSION OR CUSTOMERS
 
-pal:SetNewInfo( {"you","|pal:NRT( 'feel' )|","|pal:NRT( 'feeling' )|","|pal:NRT( 'doing' )|","|pal:NRT( 'holding' )|"}, {}, {-0.70,0}, nil, nil, 0, {"I am feeling |pal:GetEmotiveClass()|.","I feel like I am |pal:GetEmotiveClass()|.","I am feeling |pal:GetEmotiveClass()| user.","I feel like I am |pal:GetEmotiveClass()| user."}, nil, nil, nil )
+pal:SetNewInfo( {"you","|pal:NRT( 'feel' )|","|pal:NRT( 'feeling' )|","|pal:NRT( 'doing' )|","|pal:NRT( 'holding' )|"}, nil, nil, nil, nil, nil, {"I am feeling |pal:GetEmotiveClass()|.","I feel like I am |pal:GetEmotiveClass()|.","I am feeling |pal:GetEmotiveClass()| user.","I feel like I am |pal:GetEmotiveClass()| user."}, nil, nil, nil )
 pal:SetNewInfo( {"|pal:EmotionLevelEqualsOr( {3,3}, {2,3}, {3,2} )|","you","|pal:NRT( 'like' )|","|pal:NRT( 'enjoy' )|","me",}, nil, nil, false, nil, nil, {"Yer I like talking to you user.","It is nice to have you around.","It is nice to have you around user.","i do enjoy our talking."}, nil, nil, nil )
 pal:SetNewInfo( {"|pal:EmotionLevelNotEqualsOr( {3,3}, {2,3}, {3,2} )|","you","|pal:NRT( 'like' )|","|pal:NRT( 'enjoy' )|","me",}, nil, {-0.70,-0.20}, nil, nil, nil, {"I do not like you at the moment.","No","No user.","I don't like you.","I don't like you user.","What do you think.","What do you think user."}, nil, nil, nil )
 pal:SetNewInfo( {"|pal:EmotionLevelEqualsOr( {3,3}, {2,3}, {3,2} )|","you","|pal:NRT( 'hate' )|","|pal:NRT( 'dislike' )|","|pal:NRT( 'loth' )|","me",}, nil, nil, false, nil, nil, {"No no no not at all","Hevens no.","I do not dislike you user.","No I do like you.","No I do like you user."}, nil, nil, nil )
